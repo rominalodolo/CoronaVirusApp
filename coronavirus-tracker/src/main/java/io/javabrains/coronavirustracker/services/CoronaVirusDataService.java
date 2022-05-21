6a2ed28b7c379fd4b0f08c19f16db1916fd991de
rominalodolo <lodoloromina@gmail.com>
2022-05-21 17:21:29 +0200

    update

--- coronavirus-tracker/src/main/java/io/javabrains/coronavirustracker/services/CoronaVirusDataService.java
+++ coronavirus-tracker/src/main/java/io/javabrains/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,4 +1,15 @@
 package io.javabrains.coronavirustracker.services;
 
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
 public class CoronaVirusDataService {
+
+    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+
+    public void fetchVirusData() {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest.newBuilder()
+
+    }
 }
