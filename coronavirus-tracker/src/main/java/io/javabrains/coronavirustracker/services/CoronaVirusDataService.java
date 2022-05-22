4bdf5a5dafffaa1e502dea90118f242abca6b533
rominalodolo <lodoloromina@gmail.com>
2022-05-22 14:07:45 +0200

    update

--- coronavirus-tracker/src/main/java/io/javabrains/coronavirustracker/services/CoronaVirusDataService.java
+++ coronavirus-tracker/src/main/java/io/javabrains/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,5 +1,6 @@
 package io.javabrains.coronavirustracker.services;
 
+import io.javabrains.coronavirustracker.models.LocationStats;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,15 +13,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CoronaVirusDataService {
 
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+    private List<LocationStats> allStats = new ArrayList<>();
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData() throws IOException, InterruptedException  {
+        List<LocationStats> newStats = new ArrayList<>();
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(VIRUS_DATA_URL))
