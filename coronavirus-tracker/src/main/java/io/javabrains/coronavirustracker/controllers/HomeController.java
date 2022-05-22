794745bb134cf02c74afc5cb1cd43704f7f61612
rominalodolo <lodoloromina@gmail.com>
2022-05-22 17:39:11 +0200

    update

--- coronavirus-tracker/src/main/java/io/javabrains/coronavirustracker/controllers/HomeController.java
+++ coronavirus-tracker/src/main/java/io/javabrains/coronavirustracker/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package io.javabrains.coronavirustracker.controllers;
 
+import io.javabrains.coronavirustracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("testName", "TEST");
+        model.addAttribute("locationStats", coronaVirusDataService.;
         return "home";
     }
 }
