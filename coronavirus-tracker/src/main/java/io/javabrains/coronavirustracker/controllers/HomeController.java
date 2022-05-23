2b18565389949f74911fe37aef201f55256461fa
rominalodolo <lodoloromina@gmail.com>
2022-05-23 11:55:42 +0200

    update

--- coronavirus-tracker/src/main/java/io/javabrains/coronavirustracker/controllers/HomeController.java
+++ coronavirus-tracker/src/main/java/io/javabrains/coronavirustracker/controllers/HomeController.java
@@ -19,9 +19,13 @@ public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
         List<LocationStats> allStats = coronaVirusDataService.getAllStats();
-        int totalCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
 
         model.addAttribute("locationStats", coronaVirusDataService.getAllStats());
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
         return "home";
     }
 }
