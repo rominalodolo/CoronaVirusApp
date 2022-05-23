c6ddf6b39722eab397af574ac86b8fc6d5733a17
rominalodolo <lodoloromina@gmail.com>
2022-05-23 12:31:46 +0200

    update

--- coronavirus-tracker/src/main/java/io/javabrains/coronavirustracker/CoronavirusTrackerApplication.java
+++ coronavirus-tracker/src/main/java/io/javabrains/coronavirustracker/CoronavirusTrackerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class CoronavirusTrackerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CoronavirusTrackerApplication.class, args);
 	}
 
